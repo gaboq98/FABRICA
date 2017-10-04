00252e41a75807fe2af8210c845540e5a846e7e1
Gabriel Quesada <gaboq98@gmail.com>
2017-10-04 00:29:35 -0600

    Revert "Antes de poner hilos"
    This reverts commit 799032c7d5a940212d196767c36936985a3f1cea.

--- Fabrica/mainwindow.cpp
+++ Fabrica/mainwindow.cpp
@@ -12,9 +12,3 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
-
-void MainWindow::on_pushButton_clicked()
-{
-    config.setModal(true);
-    config.exec();
-}
--- Fabrica/mainwindow.h
+++ Fabrica/mainwindow.h
@@ -1,6 +1,5 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-#include "configuracion.h"
 
 #include <QMainWindow>
 
@@ -16,12 +15,8 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-private slots:
-    void on_pushButton_clicked();
-
 private:
     Ui::MainWindow *ui;
-    Configuracion config;
 };
 
 #endif // MAINWINDOW_H
